d184a92eec9581e7746320021e958a4bc9181511
andrewskr90 <andrewskr90@gmail.com>
2022-06-22 12:49:30 -0700

    removed trainer auth route, added protected route /gym-leader. Imported gym leader styles.

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,16 @@ import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import LoginPage from './components/common/LoginPage'
 import TrainerHome from './components/trainer/TrainerHome'
 import GymLeaderHome from './components/gymLeader/GymLeaderHome'
-import TrainerRoute from './utils/auth/TrainerRoute'
-import OnlyUnauthorized from './utils/auth/OnlyUnauthorized'
+import GymLeaderRoute from './utils/auth/GymLeaderRoute'
 
 import './styles/App.css'
+import './styles/GymLeader.css'
 import BillsPcService from './api/bills-pc'
+
+//calling api before app renders can lead to bugs
+//render app first, call data in useEffect,
+//then set initialData to true
+let initialData = false
 
 const App = (props) => {
     const [userClaims, setUserClaims] = useState('')
@@ -18,22 +23,29 @@ const App = (props) => {
         const checkAuth = async () => {
             await BillsPcService.authenticateSession()
                 .then(res =>  {
+                    initialData = true
                     setUserClaims(res.data)
-                    navigate('/')
                 }).catch(err => {
+                    initialData = true
                     navigate('/login')
                 })
         }
         checkAuth()
     }, [])
+
     return (<>
+        {initialData
+        ?
         <Routes>
             <Route path='/login' element={<LoginPage setUserClaims={setUserClaims} />} />
-            {/*Trainer protected routes*/}
-            <Route path='/' element={<TrainerHome />} />
+            <Route path='/' element={<TrainerHome userClaims={userClaims} />} />
             {/* Gym Leader protected routes */}
-            <Route path='/gym-leader' element={<GymLeaderHome userClaims={userClaims} />} />
+            <Route element={<GymLeaderRoute userClaims={userClaims} />} >
+                <Route path='/gym-leader' element={<GymLeaderHome userClaims={userClaims} />} />
+            </Route>
         </Routes>
+        :
+        <h2>Loading...</h2>}
     </>)
 }
 
