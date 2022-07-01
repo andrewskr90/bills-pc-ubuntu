785540f6e79a9f17e65fb70920f1f8a16ff6906d
andrewskr90 <andrewskr90@gmail.com>
2022-07-01 14:12:47 -0700

    upgrade node version to 16.14.2. Replaced bcrypt with bcrypt-nodejs

--- packages/service/middleware/auth-middleware.js
+++ packages/service/middleware/auth-middleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt-nodejs')
 
 const isOnlyLetters = (string) => {
     return /^[a-zA-Z]+$/.test(string)
