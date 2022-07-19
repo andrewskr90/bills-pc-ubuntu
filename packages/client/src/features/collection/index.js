658d982628d873f038c0f7b799d899eaef92b85a
andrewskr90 <andrewskr90@gmail.com>
2022-07-18 20:05:57 -0700

    added checks in saleNotes and collectedCardNotes queries, checking if length of resources to be added was zero

--- packages/client/src/features/collection/index.js
+++ packages/client/src/features/collection/index.js
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 const Collection = (props) => {
     const { collectedItems } = props
+
+    useEffect(() => {
+        console.log('hereeeee')
+    }, [])
+    
     return (<div className='trainerHome page'>
         {collectedItems.length > 0
         ?
--- packages/service/db/queries/collectedCardNoteQueries.js
+++ packages/service/db/queries/collectedCardNoteQueries.js
@@ -3,21 +3,26 @@ const QueryFormatters = require('../../utils/QueryFormatters')
 
 const addCollectedCardNotesMySQL = async (req, res, next) => {
     const collectedCardNotes = req.collectedCardNotes
-    const queryString = QueryFormatters.objectsToInsert(collectedCardNotes, 'collected_card_notes')
-    const query = new Promise((resolve, reject) => {
-        connection.query(queryString, (err, results) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(results)
-            }
+    if (collectedCardNotes.length > 0) {
+        const queryString = QueryFormatters.objectsToInsert(collectedCardNotes, 'collected_card_notes')
+        const query = new Promise((resolve, reject) => {
+            connection.query(queryString, (err, results) => {
+                if (err) {
+                    reject(err)
+                } else {
+                    resolve(results)
+                }
+            })
         })
-    })
-    try {
-        req.addCollectedCardNotesResults = await query
-        return next()
-    } catch (err) {
-        next(err)
+        try {
+            req.addCollectedCardNotesResults = await query
+            return next()
+        } catch (err) {
+            next(err)
+        }
+    } else {
+        req.addCollectedCardNotesResults = { affectedRows: 0 }
+        next()
     }
 }
 
